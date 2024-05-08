624546cb5690de68bbee759d291a7ff47b157f5a
Alexander Baronsky <105839199+ABaronCode@users.noreply.github.com>
2024-05-08 01:14:32 -0400

    main,jss,css and node

---
+++ main.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  document.getElementById('Dropdown').classList.toggle('show');
+  document.getElemendById('.overlay');
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+    document.body.classList.toggle('blur');
+}
