12a3c5c2b4b06433bc14df9eb392b35fe9b60dfc
Alexander Baronsky <105839199+ABaronCode@users.noreply.github.com>
2024-06-13 21:06:45 -0400

    Add files via upload
    Rough Draft #2

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
+let topButton = document.getElementById("goUp");
+
+window.onload = function() {
+  initiallyHide();
+}
+window.onscroll = function() {
+  hide_toTop();
+}
+
 function toggleMenu() {
   document.getElementById('Dropdown').classList.toggle('show');
   document.getElemendById('.overlay');
+}
+
+function initiallyHide() {
+  topButton.style.display = "none";
+}
+
+function hide_toTop() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topButton.style.display = "block";
+  }else {
+    topButton.style.display = "none";
+  }
+}
+// Go to top of page
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -15,5 +41,4 @@ window.onclick = function(event) {
         }
       }
     }
-    document.body.classList.toggle('blur');
 }
