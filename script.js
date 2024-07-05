d52c4e7449abb9f29d5eeb89c275136f74293aec
Alexander Baronsky <105839199+ABaronCode@users.noreply.github.com>
2024-07-04 21:35:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-let topButton = document.getElementById("goUp");
+let topButton = document.getElementById("go-up");
 var iframeResume = document.getElementById("pdf");
 
 window.onload = function() {
   initiallyHide();
   // adjust iframe
-iframeResume.width = iframe.contentWindow.document.body.scrollWidth + 'px';
-iframeResume.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+  iframeResume.width = iframe.contentWindow.document.body.scrollWidth + 'px';
+  iframeResume.height = iframe.contentWindow.document.body.scrollHeight + 'px';
 }
+/*
+//This is for animating the index.html article boxes on scroll
+const observer = new IntersectionObserver(entries => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isInterseting) {
+      entry.target.classList.add("clear")
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));*/
+
+// To the top button
 window.onscroll = function() {
   hide_toTop();
 }
-
-function toggleMenu() {
-  document.getElementById('Dropdown').classList.toggle('show');
-  document.getElemendById('.overlay');
-}
-
+// Starts hidden
 function initiallyHide() {
   topButton.style.display = "none";
 }
-
+// Hide to top of page
 function hide_toTop() {
   if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
     topButton.style.display = "block";
@@ -33,6 +42,11 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+//Burger Menu Javascript
+function toggleMenu() {
+  document.getElementById('Dropdown').classList.toggle('show');
+  document.getElemendById('.overlay');
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -47,3 +61,15 @@ window.onclick = function(event) {
     }
 
 }
+
+//implementt reduced motion behaviors
+//let motionQuery = matchMedia('(prefers-reduced-motion)');
+
+//const handleReduceMotionChanged = () => {
+  //if (motionQuery.matches){
+//
+  //}
+//}
+
+//motionQuery.addListener(handleReduceMotionChanged);
+//handleReduceMotionChanged()
